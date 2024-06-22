430b27968956fd5f601a2036a07224d3e289f78c
Alex LaFroscia <alex@lafroscia.com>
2024-06-22 14:18:12 -0400

    better error handling while compiling

--- bin/dev.ts
+++ bin/dev.ts
@@ -1,5 +1,18 @@
 import { build, TEMPLATE_DIR, write } from "./build.ts";
 
+async function* throttle<T>(iter: AsyncIterable<T>, diff: number = 10) {
+  let lastEmit: number = 0;
+
+  for await (const i of iter) {
+    const now = Date.now();
+
+    if (now - lastEmit > diff) {
+      lastEmit = now;
+      yield i;
+    }
+  }
+}
+
 // Perform an initial build
 await write(await build());
 console.log(`✅ Theme updated!`);
@@ -7,7 +20,12 @@ console.log(`✅ Theme updated!`);
 // Watch template directory and rebuild on change
 const watcher = Deno.watchFs(TEMPLATE_DIR);
 
-for await (const _event of watcher) {
-  await write(await build());
-  console.log(`✅ Theme updated!`);
+for await (const _event of throttle(watcher)) {
+  try {
+    await write(await build());
+    console.log(`✅ Theme updated!`);
+  } catch (e) {
+    console.log(`❌ Error compiling theme!`);
+    console.error(e);
+  }
 }
